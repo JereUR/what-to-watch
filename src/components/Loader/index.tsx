0ff441ee212b2deaaaa2523b6e19b715958452bc
JereUR <jeremias.jdv@gmail.com>
2023-11-05 22:52:19 -0300

    feat(Root): Loader spinner component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,55 @@
+import styled, { keyframes } from 'styled-components'
+
+const Loader = () => {
+  return (
+    <StyledLoader>
+      <div />
+      <div />
+      <div />
+      <div />
+    </StyledLoader>
+  )
+}
+
+export default Loader
+
+const ldsRing = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const StyledLoader = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin-top: 2rem;
+    border: 8px solid var(--header-words);
+    border-radius: 50%;
+    animation: ${ldsRing} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: var(--header-words) transparent transparent transparent;
+  }
+
+  div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  .div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`
