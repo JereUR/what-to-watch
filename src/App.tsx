dc0010d9f6d276b7d44d18e1160a229b343233ad
JereUR <jeremias.jdv@gmail.com>
2023-11-05 22:51:35 -0300

    feat(Root): Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components'
 import './App.css'
-import { Header } from './components/Header'
+
 import Footer from './components/Footer'
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <MainContainer>
       <Header />
+      <Home />
       <Footer />
     </MainContainer>
   )
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+import Loader from '../Loader'
+import MediaForm from './MediaForm'
+
+export default function Home() {
+  const handleClickScroll = () => {
+    document.querySelector('.header')?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  return (
+    <StyledHome>
+      <article>
+        <MediaForm />
+        <Loader />
+        {/* {search != null && !loading && <InfoTable data={data} />} */}
+      </article>
+      <button
+        onClick={handleClickScroll}
+        className="scroll-top-btn hidden"
+        type="button"
+      >
+        &#11014;
+      </button>
+    </StyledHome>
+  )
+}
+
+const StyledHome = styled.div`
+  .scroll-top-btn {
+    position: fixed;
+    z-index: 999;
+    margin-bottom: 100px;
+    bottom: 1vh;
+    right: calc(4.25rem + 1vw);
+    width: 6rem;
+    height: 6rem;
+    border-radius: 50%;
+    font-size: 4rem;
+    font-weight: bold;
+    background-color: var(--background-color);
+    box-shadow: 0 0 3px 3px var(--main-color);
+    color: var(--main-color);
+    cursor: pointer;
+    outline: 0;
+    border: 0;
+    transition: all 0.3s ease-out;
+
+    &:hover {
+      color: var(--third-color);
+      background-color: var(--main-color);
+      box-shadow: 0 0 3px 3px var(--third-color);
+    }
+  }
+`
