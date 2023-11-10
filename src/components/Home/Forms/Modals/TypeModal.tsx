f1250bb0e141ee42c4d266c5ddee0ab6e8af62c4
JereUR <jeremias.jdv@gmail.com>
2023-11-10 20:27:31 -0300

    feat(Root): Type modal form component added

---
+++ src/components/Home/Forms/Modals/TypeModal.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import Modal from '../../../Modal'
+import { TYPES } from '../FormItems.tsx/TYPES'
+import Content from './Content'
+
+interface Props {
+  filmTypeState: boolean
+  setFilmTypeState: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function TypeModal({ filmTypeState, setFilmTypeState }: Props) {
+  return (
+    <Modal
+      state={filmTypeState}
+      setState={setFilmTypeState}
+      title="SELECT TYPE OF FILM"
+    >
+      <Content>
+        <StyledType>
+          <div className="type-section">
+            {TYPES.map((type) => (
+              <div key={type.id} className="input-container">
+                <input
+                  type="checkbox"
+                  value={type.value}
+                  id={type.id}
+                  className="type-input"
+                  /* defaultChecked={types.includes(type.value)} */
+                />
+                <label htmlFor={type.id}>{type.label}</label>
+              </div>
+            ))}
+          </div>
+        </StyledType>
+      </Content>
+    </Modal>
+  )
+}
+
+const StyledType = styled.div`
+  margin: 0 auto;
+
+  .type-section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+
+    label {
+      display: inline;
+      font-weight: 600;
+      color: var(--words-color);
+      margin-bottom: 11px;
+      width: 70vh;
+      float: left;
+      cursor: pointer;
+      padding: 0 0.8rem;
+      box-sizing: border-box;
+      margin: 1rem;
+      font-size: 24px;
+      font-weight: bold;
+      border-radius: 100px;
+      transition: all 0.5s ease 0s;
+      background: var(--third-color);
+      box-shadow: 0 0 1px 1px var(--main-color);
+    }
+
+    label:hover {
+      transform: scale(1.02);
+    }
+
+    input + label:before {
+      content: '';
+      width: 30px;
+      height: 30px;
+      float: left;
+      margin-right: 0.5em;
+      margin-left: 0.4em;
+      border: 2px solid #ccc;
+      background: #fff;
+      margin-top: 0.7em;
+    }
+  }
+`
