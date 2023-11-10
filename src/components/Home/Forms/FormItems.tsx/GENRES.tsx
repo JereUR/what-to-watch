8b83cbf6d2f183709fb5b268f9fcf2832b05a1b7
JereUR <jeremias.jdv@gmail.com>
2023-11-10 20:26:36 -0300

    feat(Root): Files location changed

---
+++ src/components/Home/Forms/FormItems.tsx/GENRES.tsx
@@ -0,0 +1,127 @@
+export const GENRES = [
+  {
+    id: 'action',
+    value: 'action',
+    label: 'Action'
+  },
+  {
+    id: 'adventure',
+    value: 'adventure',
+    label: 'Adventure'
+  },
+  {
+    id: 'animation',
+    value: 'animation',
+    label: 'Animation'
+  },
+  {
+    id: 'biography',
+    value: 'biography',
+    label: 'Biography'
+  },
+  {
+    id: 'comedy',
+    value: 'comedy',
+    label: 'Comedy'
+  },
+  {
+    id: 'crime',
+    value: 'crime',
+    label: 'Crime'
+  },
+  {
+    id: 'documentary',
+    value: 'documentary',
+    label: 'Documentary'
+  },
+  {
+    id: 'drama',
+    value: 'drama',
+    label: 'Drama'
+  },
+  {
+    id: 'family',
+    value: 'family',
+    label: 'Family'
+  },
+  {
+    id: 'fantasy',
+    value: 'fantasy',
+    label: 'Fantasy'
+  },
+  {
+    id: 'film_noir',
+    value: 'film_noir',
+    label: 'Film Noir'
+  },
+  {
+    id: 'game_show',
+    value: 'game_show',
+    label: 'Game Show'
+  },
+  {
+    id: 'history',
+    value: 'history',
+    label: 'History'
+  },
+  {
+    id: 'horror',
+    value: 'horror',
+    label: 'Horror'
+  },
+  {
+    id: 'music',
+    value: 'music',
+    label: 'Music'
+  },
+  {
+    id: 'musical',
+    value: 'musical',
+    label: 'Musical'
+  },
+  {
+    id: 'mystery',
+    value: 'mystery',
+    label: 'Mystery'
+  },
+  {
+    id: 'reality_tv',
+    value: 'reality_tv',
+    label: 'Reality TV'
+  },
+  {
+    id: 'romance',
+    value: 'romance',
+    label: 'Romance'
+  },
+  {
+    id: 'sci_fi',
+    value: 'sci_fi',
+    label: 'Science Fiction'
+  },
+  {
+    id: 'sport',
+    value: 'sport',
+    label: 'Sport'
+  },
+  {
+    id: 'talk_show',
+    value: 'talk_show',
+    label: 'Talk-Show'
+  },
+  {
+    id: 'thriller',
+    value: 'thriller',
+    label: 'Thriller'
+  },
+  {
+    id: 'war',
+    value: 'war',
+    label: 'War'
+  },
+  {
+    id: 'western',
+    value: 'western',
+    label: 'Western'
+  }
+]
