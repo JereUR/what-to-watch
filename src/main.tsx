63240481152e28b97f2d786846ea2216c55b1266
JereUR <jeremias.jdv@gmail.com>
2023-11-10 20:25:37 -0300

    feat(Root): Redux store configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
 import App from './App.tsx'
 import './index.css'
+import { store } from './store/index.ts'
 
 const rootElement = document.getElementById('root')
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   )
 } else {
   console.error("Error to find DOM with id 'root'.")
