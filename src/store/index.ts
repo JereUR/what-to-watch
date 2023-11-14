b2350373ea066c2d0dc0799cfa78d9e8b1d5c119
JereUR <jeremias.jdv@gmail.com>
2023-11-13 21:41:20 -0300

    feat(Root): Configure store with search reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import searchReducer from './search/slice'
+
 export const store = configureStore({
-  reducer: {}
+  reducer: { search: searchReducer }
 })
 
 export type RootState = ReturnType<typeof store.getState>
