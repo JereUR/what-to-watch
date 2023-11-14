c6eff77211d82ce71dede256f932de32febd22f2
JereUR <jeremias.jdv@gmail.com>
2023-11-13 21:41:01 -0300

    feat(Root): Hook for managment of search state reducer

---
+++ src/hooks/useSearchActions.ts
@@ -0,0 +1,46 @@
+import {
+  addNewGenre,
+  addNewType,
+  removeGenre,
+  removeType,
+  updateTop,
+  updateVotes
+} from '../store/search/slice'
+import { useAppDispatch } from './store'
+
+export const useSearchActions = () => {
+  const dispatch = useAppDispatch()
+
+  const addType = (value: string) => {
+    dispatch(addNewType(value))
+  }
+
+  const removeTypeItem = (value: string) => {
+    dispatch(removeType(value))
+  }
+
+  const addGenre = (value: string) => {
+    dispatch(addNewGenre(value))
+  }
+
+  const removeGenreItem = (value: string) => {
+    dispatch(removeGenre(value))
+  }
+
+  const updateExtraVotes = (value: string) => {
+    dispatch(updateVotes(value))
+  }
+
+  const updateExtraTop = (value: string) => {
+    dispatch(updateTop(value))
+  }
+
+  return {
+    addType,
+    removeTypeItem,
+    addGenre,
+    removeGenreItem,
+    updateExtraVotes,
+    updateExtraTop
+  }
+}
