bbf87f6c23d5990dfd9625edc564afd381657c58
JereUR <jeremias.jdv@gmail.com>
2023-11-16 22:26:10 -0300

    feat(Root): Send data to result component

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -1,16 +1,46 @@
 import styled from 'styled-components'
 import { useAppSelector } from '../../hooks/store'
+import Item from './Item'
 
 export default function Results() {
   const results = useAppSelector((state) => state.results)
 
-  console.log(results)
-
-  return <StyledResults>Results</StyledResults>
+  return (
+    <StyledResults>
+      {results.data.length > 0 ? (
+        results.data.map((item, index) => (
+          <Item item={item} index={index + 1} />
+        ))
+      ) : (
+        <div className="no-data">
+          <hr />
+          <h1>No Matches</h1>
+        </div>
+      )}
+    </StyledResults>
+  )
 }
 
 const StyledResults = styled.div`
   padding: 0 1rem 0 1rem;
   padding-top: 5rem;
   color: var(--header-words);
+
+  .no-data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: -3rem;
+
+    hr {
+      margin-bottom: 3rem;
+    }
+
+    h1 {
+      color: var(--header-words);
+      text-align: center;
+      font-size: 3rem;
+      font-weight: bold;
+    }
+  }
 `
